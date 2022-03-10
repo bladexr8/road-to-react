17ca053e0083f09a75fbc2e07a28476b5933e159
Stephen Moss <bladexr8@gmail.com>
2022-03-10 14:31:44 +1100

    finished "React State"

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 
 const  App = () => {
 
@@ -44,15 +44,23 @@ const  App = () => {
 // Search Component
 const Search = () => {
 
+  // state property and setter function
+  // using React useState() hook
+  const [searchTerm, setSearchTerm] = React.useState('');
+
   // handle text input
   const handleChange = (event) => {
-    console.log(event);
+    setSearchTerm(event.target.value);
   }
 
   return (
     <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
     </div>
   )
 }
